9f08a65fe439bad574341163695b7925d5cbd671
Ziff Urag <zurag@student.21-school.ru>
2022-07-03 21:02:38 +0300

    added base vector

--- test_vec.cpp
+++ test_vec.cpp
@@ -2,6 +2,7 @@
 #include "vector.hpp"
 #include <assert.h>
 #include <iostream>
+#include <typeinfo>
 #define assertm(exp, msg) assert(((void)msg, exp))
 
 
@@ -36,7 +37,6 @@ public:
 	}
 };
 
-
 class B {
 public:
 	int val;
@@ -55,22 +55,163 @@ void ConstructorTest() {
 
 	ft::vector<int> fvec1;
 	std::vector<int> svec1;
+	assertm(fvec1.empty() == svec1.empty(), "Default constructor check empty");
 	assertm(fvec1.size() == svec1.size(), "Default constructor check size");
 	assertm(fvec1.capacity() == svec1.capacity(), "Default constructor check capacity");
 
-	
 	ft::vector<B> fvec2(100, B(20));
 	std::vector<B> svec2(100, B(20));
+
 	for (size_t i = 0; i < 100; ++i) {
 		assertm(svec2[i].val == fvec2[i].val, "Fill Constructor check value");
 	}
 	assertm(fvec2.size() == svec2.size(), "fill constructor check size");
+	assertm(fvec2.empty() == svec2.empty(), "fill constructor check empty");
+	assertm(fvec2.max_size() == svec2.max_size(), "fill constructor check max size");
 	assertm(fvec2.capacity() == svec2.capacity(), "fill constructor check capacity");
-
-	ft::vector<A> fvec3(100); // не работает если у класса нет коструктора копирования и аргумент не передается 
+	
+	ft::vector<A> fvec3(100);
 	std::vector<A> svec3(100);
 
+	ft::vector<B> fvec4(fvec2);
+	std::vector<B> svec4(svec2);
+	
+	for (size_t i = 0; i < 100; ++i) {
+		assertm(svec4[i].val == fvec4[i].val, "Copy Constructor check value");
+	}
+	assertm(fvec4.size() == svec4.size(), "Copy Constructor check size");
+	assertm(fvec4.max_size() == svec4.max_size(), "Copy Constructor check max size");
+	assertm(fvec4.capacity() == svec4.capacity(), "Copy Constructor check capacity");
+	
+	ft::vector<B> fvec5(100, B(30));
+	std::vector<B> svec5(100, B(30));
+	fvec5 = fvec4;
+	svec5 = svec4;
+	for (size_t i = 0; i < 100; ++i) {
+
+		assertm(svec5[i].val == fvec5[i].val, "Operator = check value");
+	}
+	assertm(fvec5.size() == svec5.size(), "Operator = check size");
+	assertm(fvec5.max_size() == svec5.max_size(), "Operator =  check max size");
+	assertm(fvec5.capacity() == svec5.capacity(), "Operator = check capacity");
 }
+
+
+void ResizeTest() {
+	ft::vector<int> fvec(10, 1);
+	std::vector<int> svec(10, 1);
+	fvec.resize(22, 2);
+	svec.resize(22, 2);
+	for (int i = 0; i < svec.size(); ++i) {
+		assertm(svec[i] == fvec[i], "Resize check value");
+	}
+	assertm(fvec.size() == svec.size(), "Resize check size");
+	assertm(fvec.max_size() == svec.max_size(), "Resize check max size");
+	assertm(fvec.capacity() == svec.capacity(), "Resize check capacity");
+	
+	ft::vector<int> fvec2(10, 1);
+	std::vector<int> svec2(10, 1);
+	fvec2.resize(2, 2);
+	svec2.resize(2, 2);
+	for (int i = 0; i < svec2.size(); ++i) {
+		assertm(svec2[i] == fvec2[i], "Resize check value");
+	}
+	assertm(fvec2.size() == svec2.size(), "Resize check size");
+	assertm(fvec2.max_size() == svec2.max_size(), "Resize check max size");
+	assertm(fvec2.capacity() == svec2.capacity(), "Resize check capacity");
+	//  std::cout << fvec2.size() << " " << svec2.size() << '\n';
+}
+
+
+void ReserveTest() {
+	std::vector<int> svec;
+	ft::vector<int> fvec;
+	assertm(fvec.size() == svec.size(), "Reserve check size");
+	svec.reserve(1);
+	assertm(fvec.size() == svec.size(), "Reserve check size");
+	svec.reserve(10);
+	assertm(fvec.size() == svec.size(), "Reserve check size");
+	svec.reserve(7);
+	assertm(fvec.size() == svec.size(), "Reserve check size");
+	svec.reserve(14);
+	assertm(fvec.size() == svec.size(), "Reserve check size");
+
+	std::vector<int> svec2(2, 1);
+	ft::vector<int> fvec2(2, 1);
+	svec.reserve(14);
+	fvec.reserve(14);
+	for (size_t i = 0; i < svec2.size();  ++i) {
+		assertm(fvec2[i] == svec2[i], "Reserve check old value");
+	}
+	assertm(fvec.size() == svec.size(), "Reserve check size");
+}
+
+
+void AtTest() {
+	std::vector<int> svec(10);
+	svec.reserve(30);
+	try {
+		svec.at(10)=100;      // vector::at throws an out-of-range
+	}
+	catch (const std::out_of_range& oor) {
+		std::cerr << "Out of Range error: " << oor.what() << '\n';
+	}
+	ft::vector<int> fvec(10);
+	try {
+		fvec.at(20)=100;
+	}
+	catch (const std::out_of_range& oor) {
+		std::cerr << "Out of Range error: " << oor.what() << '\n';
+	}
+
+
+}
+
+
+void FrontBackTest(){
+	std::vector<int> svec(10, 1);
+	ft::vector<int> fvec(10, 1);
+	svec.push_back(2);
+	fvec.push_back(2);
+	assertm(fvec.front() == svec.front(), "Front check value");
+	assertm(fvec.back() == svec.back(), "Back check value");
+	
+}
+
+void Push_backTest()
+{
+	std::vector<int> svec;
+	std::vector<int> fvec;
+	for (size_t i = 0; i < 10; ++ i) {
+		svec.push_back(i);
+	}
+	for (size_t i = 0; i < 10; ++ i) {
+		fvec.push_back(i);
+	}
+	for (size_t i = 0; i < svec.size();  ++i) {
+		assertm(fvec[i] == svec[i], "Push_back check value");
+	}
+	assertm(fvec.size() == svec.size(), "Push_back check size");
+	assertm(fvec.capacity() == svec.capacity(), "Push_back check size");
+
+	std::vector<int> svec2(1);
+	std::vector<int> fvec2(1);
+	for (size_t i = 0; i < 10; ++ i) {
+		svec2.push_back(i);
+	}
+	for (size_t i = 0; i < 10; ++ i) {
+		fvec2.push_back(i);
+	}
+	for (size_t i = 0; i < svec2.size();  ++i) {
+		assertm(fvec2[i] == svec2[i], "Push_back check value");
+	}
+	assertm(fvec2.size() == svec2.size(), "Push_back check size");
+	assertm(fvec2.capacity() == svec2.capacity(), "Push_back check size");
+}
+
+
+
+
 
 
 int main()
@@ -79,5 +220,15 @@ int main()
 	std::cout << "Typedef test OK\n";
 	ConstructorTest();
 	std::cout << "Constructor test OK\n";
+	ResizeTest();
+	std::cout << "Resize test OK\n";
+	ReserveTest();
+	std::cout << "Reserve test OK\n";
+	AtTest();
+	std::cout << "at test OK\n";
+	FrontBackTest();
+	std::cout << "FrontBackTest OK\n";
+	Push_backTest();
+	std::cout << "Push_backTest OK\n";
  	return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <cstddef>
 #include <iostream>
+#include <stdexcept>
 
 namespace ft
 {
@@ -38,25 +39,75 @@ namespace ft
 			_size = 0;
 		}
 
-		explicit vector (size_type n, const value_type& val = value_type(),
+		explicit vector (size_type n, const value_type& val,
 						const allocator_type& alloc = allocator_type()):_alloc(alloc) {
-			_capacity = n;
-			_size = n;
-			_arr = _alloc.allocate(n);
-			for (size_t i = 0; i < n; ++i) {
-				_alloc.construct(_arr + i, val);
+			if (n > 0) {
+				_arr = _alloc.allocate(n);
+				_capacity = n;
+				for (_size = 0; _size < n; ++_size) {
+					_alloc.construct(_arr + _size, val);
+				}
+				_size = n;
+			}
+			else {
+				_arr = NULL;
+				_capacity = 0;
+				_size = 0;
 			}
 		}
 
-		// vector (const vector& x){
-		// 	_capacity = x.capacity();
-		// 	_size = x.size();
-		// 	_arr = _alloc.allocate(x.capacity());
-		// 	for (size_t i; i < _size; ++i) {
-		// 		_alloc.construct(_arr + i, val);
-		// 	}
+		explicit vector (size_type n,
+						const allocator_type& alloc = allocator_type()):_alloc(alloc) {
+			if (n > 0) {
+				_arr = _alloc.allocate(n);
+				_capacity = n;
+				for (_size = 0; _size < n; ++_size) {
+					_alloc.construct(_arr + _size);
+				}
+				_size = n;
+			}
+			else {
+				_arr = NULL;
+				_capacity = 0;
+				_size = 0;
+			}
+		}
 
-		// }
+		vector (const vector& x): _alloc(x.get_allocator()){
+			_size = 0;
+			_arr = NULL;
+			size_t x_size = x.size();
+
+			if (x.capacity() > 0) {
+				_arr = _alloc.allocate(x.capacity());
+				for (_size = 0; _size < x_size; ++_size) {
+					_alloc.construct(_arr + _size, x[_size]);
+				}
+				_size = x_size;
+			}
+			_capacity = x.capacity();
+		}
+
+		vector& operator= (const vector& x) {
+			size_t x_size = x.size();
+			if (_capacity > 0) {
+				for (size_t i = 0; i < _size; ++i) {
+					_alloc.destroy(_arr + i);
+				}
+				_alloc.deallocate(_arr, _capacity);
+			}
+			if (x.capacity() > 0) {
+				_arr = _alloc.allocate(x.capacity());
+				for (_size = 0; _size < x_size; ++_size) {
+					_alloc.construct(_arr + _size, x[_size]);
+				}
+			}
+			else
+				_arr = NULL;
+			_capacity = x.capacity();
+			_size = x.size();
+			return *this;
+		}
 
 		~vector() {
 			if (_capacity) {
@@ -65,27 +116,68 @@ namespace ft
 				}
 				_alloc.deallocate(_arr, _capacity);
 			}
+			_capacity = 0;
+			_size = 0;
 		}
 
 		size_type size() const {
 			return _size;
 		}
 
-
-		// size_type max_size() const {
-		// }
+		size_type max_size() const {
+			return _alloc.max_size();
+		}
 
 		size_type capacity() const {
 			return _capacity;
 		}
 
-		// void resize(size_type count, T value = T() ) {
-		// 	if (count > _capacity) {
-		// 		//выделяем заново память и копируем элементы
-		// 	}
-		// 	else
-		// 		_size = count;
-		// }
+		bool empty() const {
+			return (_size == 0);
+		}
+
+		void resize(size_type count, T value = T()) {
+
+			if (count > _capacity) {
+				size_t	mem = _capacity * 2;
+				if (count > mem)
+					mem = count;
+				this->reserve(mem);
+				for (; _size < count; ++_size) {
+					_alloc.construct(_arr + _size, value);
+				}
+			}
+			else if (count >_size && count < _capacity) {
+				for (; _size < count; ++_size) {
+					_alloc.construct(_arr + _size, value);
+				}
+			}
+			else if (count < _size) {
+				for (size_t i = _size - 1; i > count; --i) {
+					_alloc.destroy(_arr + i);
+				}
+				_size = count;
+			}
+		}
+
+		void reserve (size_type n) {
+			T *tmp = NULL;
+
+			if (n > _capacity) {
+				tmp = _alloc.allocate(n);
+				for (size_t i = 0; i < _size; ++i) {
+					_alloc.construct(tmp + i, _arr[i]);
+				}
+				if (_capacity > 0) {
+					for (size_t i = 0; i < _size; ++i) {
+						_alloc.destroy(_arr + i);
+					}
+					_alloc.deallocate(_arr, _capacity);
+				}
+				_capacity = n;
+				_arr = tmp;
+			}
+		}
 
 		reference operator[] (size_type n) {
 			return *(_arr + n);
@@ -93,6 +185,65 @@ namespace ft
 		
 		const_reference operator[] (size_type n) const {
 			return *(_arr + n);
+		}
+
+		reference at (size_type n) {
+			if (n >= _size)
+				throw std::out_of_range("vector");
+			return *(_arr + n);
+		}
+		
+		const_reference at (size_type n) const {
+			if (n >= _size)
+				throw std::out_of_range("vector");
+			return *(_arr + n);
+		}
+
+		reference front() {			
+			return _arr[0];
+		}
+		
+		const_reference front() const {
+			return _arr[0];
+		}
+
+		reference back() {
+			return _arr[_size - 1];	
+		}
+		
+		const_reference back() const {
+			return _arr[_size - 1];
+		}
+
+		void push_back(const value_type& val) {
+			if (_size == _capacity) {
+				if (!_capacity) {
+					reserve(1);
+					_capacity = 1;
+				}
+				else
+					reserve(_capacity * 2);
+			}
+			_alloc.construct(_arr + _size, val);
+			++_size;
+		}
+
+		void pop_back() {
+			_alloc.destroy(_arr + _size - 1);
+			--_size;
+		}
+
+		void clear() {
+			if (_capacity) {
+				for (size_t i = 0; i < _size; ++i) {
+					_alloc.destroy(_arr + i);
+				}
+			}
+			_size = 0;
+		}
+
+		allocator_type get_allocator() const {
+			return _alloc;
 		}
 
 	private:
